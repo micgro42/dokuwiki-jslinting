2f6d03d74f3e3a8fa5139a6db88749fd81a21af3
Michael Grosse <grosse@cosmocode.de>
2017-01-06 17:10:47 +0100

    feat:add basic grunt config and package.json

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'), // the package file to use
+    scriptfiles: ['Gruntfile.js',  'script/*.js', 'script.js'],
+    eslint: {
+      all: ['<%= scriptfiles %>']
+    },
+    watch: {
+        linting: {
+            files: ['<%= scriptfiles %>'],
+            tasks: ['eslint'],
+            options: {
+                spawn: false,
+            },
+        }
+    }
+});
+
+    grunt.event.on('watch', function(action, filepath) {
+        grunt.config('eslint.all', filepath);
+    });
+
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-eslint');
+grunt.registerTask('default', ['eslint']);
+};
+
